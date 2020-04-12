00d3e3192cb591e58079c309994792c1be2109e9
Nacho <joseignaciorm13@gmail.com>
2020-04-12 17:14:08 +0200

    Refactor backend to composite all views into one working with ViewStes from Django Rest Framework

--- backend/src/articles/api/views.py
+++ backend/src/articles/api/views.py
@@ -1,36 +1,10 @@
-from rest_framework.generics import (
-    ListAPIView, 
-    RetrieveAPIView, 
-    CreateAPIView,
-    UpdateAPIView,
-    DestroyAPIView
-)
+from rest_framework import viewsets
 
 from articles.models import Article
 from .serializers import ArticleSerializer
 
-# Article List Serializer view. Specify query Specify serializer class
-# GenericViews importing ListApiView from rest_framework.generics
-class ArticleListView(ListAPIView):
+# ViewSets
+class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
-
-# Article Detail Serializer view.
-class ArticleDetailView(RetrieveAPIView):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
-
-# Article Create Serializer view.
-class ArticleCreateView(CreateAPIView):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
-
-# Article Update Serializer view.
-class ArticleUpdateView(UpdateAPIView):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
-
-# Article Create Serializer view.
-class ArticleDeleteView(DestroyAPIView):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
+    
