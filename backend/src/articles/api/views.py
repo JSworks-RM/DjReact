f2ea0bc6509af7f08c0e31a1ab6aca2b71b53714
Nacho <joseignaciorm13@gmail.com>
2020-04-12 12:30:52 +0200

    Added DetailAPIView, CreateAPIView, UpdateAPIView and DeleteAPIView with rest_framework.generics

--- backend/src/articles/api/views.py
+++ backend/src/articles/api/views.py
@@ -1,4 +1,10 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import (
+    ListAPIView, 
+    RetrieveAPIView, 
+    CreateAPIView,
+    UpdateAPIView,
+    DestroyAPIView
+)
 
 from articles.models import Article
 from .serializers import ArticleSerializer
@@ -11,5 +17,20 @@ class ArticleListView(ListAPIView):
 
 # Article Detail Serializer view.
 class ArticleDetailView(RetrieveAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+# Article Create Serializer view.
+class ArticleCreateView(CreateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+# Article Update Serializer view.
+class ArticleUpdateView(UpdateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+# Article Create Serializer view.
+class ArticleDeleteView(DestroyAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
