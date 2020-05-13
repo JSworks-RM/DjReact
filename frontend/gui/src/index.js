c27e43409b6227eb88b07e85ed880e73f92eb356
Nacho <joseignaciorm13@gmail.com>
2020-05-13 17:19:47 +0200

    Create reducers, store, Provider app with the store.

--- frontend/gui/src/index.js
+++ frontend/gui/src/index.js
@@ -1,17 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
+import reducer from "./store/reducers/authReducers";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+
+const composeEnhances = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhances(applyMiddleware(thunk)));
+
+const app = (
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </Provider>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(app, document.getElementById("root"));
+
 serviceWorker.unregister();
